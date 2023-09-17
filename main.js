c99f9b04120d627e647a7fc803357f592b5c8f1a
luryand <lurydd@live.com>
2023-09-17 14:28:04 -0300

    tirei o js, por enquanto

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-function executeSearch() {
+/* function executeSearch() {
     const query = document.getElementById('busca').value;
     console.log(`Buscando: ${query}`);
-    // Adicione sua lógica de pesquisa aqui
   }
-  
+ */
