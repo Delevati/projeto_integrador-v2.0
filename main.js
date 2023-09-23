7fce19d80a0226d6bc49d624b680dd99851df8b2
luryand <lurydd@live.com>
2023-09-23 02:27:42 -0300

    index, my_events e sobre atualizados

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-/* function executeSearch() {
-    const query = document.getElementById('busca').value;
-    console.log(`Buscando: ${query}`);
-  }
- */
+document.addEventListener('DOMContentLoaded', function() {
+
+  var cards = document.querySelectorAll('.evento-card');
+  var keywords = ['event', 'concert', 'conference', 'festival', 'seminar'];
+
+  cards.forEach(function(card, index) {
+      var keyword = keywords[index % keywords.length];
+      var imageUrl = 'https://source.unsplash.com/300x200/?' + keyword;
+      card.style.backgroundImage = 'url(' + imageUrl + ')';
+  });
+});
