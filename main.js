71ca8a39c6010abcae7296b71627b633092d1eb2
Henriqu3al <h3nriquex.al@gmail.com>
2023-10-28 10:39:54 -0300

    validando campos de login + finalização do mesmo

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
-
 /* API imagens aleatórias */
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
+  var cards = document.querySelectorAll(".evento-card");
+  var keywords = ["event", "concert", "conference", "festival", "seminar"];
 
-  var cards = document.querySelectorAll('.evento-card');
-  var keywords = ['event', 'concert', 'conference', 'festival', 'seminar'];
-
-  cards.forEach(function(card, index) {
-      var keyword = keywords[index % keywords.length];
-      var imageUrl = 'https://source.unsplash.com/300x200/?' + keyword;
-      card.style.backgroundImage = 'url(' + imageUrl + ')';
+  cards.forEach(function (card, index) {
+    var keyword = keywords[index % keywords.length];
+    var imageUrl = "https://source.unsplash.com/300x200/?" + keyword;
+    card.style.backgroundImage = "url(" + imageUrl + ")";
   });
 });
 
+function validarLogin() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (username.trim() === "") {
+    alert("informe seu nome de usuário.");
+    return false;
+  }
+
+  if (password.trim() === "") {
+    alert("informe sua senha.");
+    return false;
+  }
+
+  return true;
+}
+
+id = "login-form";
